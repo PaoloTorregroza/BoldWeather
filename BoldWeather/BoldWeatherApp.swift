cb256f1b7cc2b6890b64887b47050ec0a2fbe4a6
PaoloTorregroza <paolinskypdtn@outlook.com>
2021-09-08 11:54:22 -0500

    Initial Commit

---
+++ BoldWeather/BoldWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoldWeatherApp.swift
+//  BoldWeather
+//
+//  Created by Paolo on 8/09/21.
+//
+
+import SwiftUI
+
+@main
+struct BoldWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoldWeather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BoldWeather
+//
+//  Created by Paolo on 8/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
